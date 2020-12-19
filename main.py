ab2e5debd32d6eb643a96c4fe8e18ccc3271e930
Tim McGinley <timwasnever@gmail.com>
2020-12-19 22:01:47 +0100

    Update main.py

--- main.py
+++ main.py
@@ -14,14 +14,23 @@ objectify.deannotate(tree, cleanup_namespaces=True)
 
 
 for obj in tree.getroot():
+
+    # this is used to get the process names for below...
+    
     # get the collaborations
     if (obj.tag =='collaboration'):
         for parti in obj:
             print('\t{} : {} | '.format(parti.get('name'),parti.get('id')))
 
+# TODO : switch pro anc object naming -> its the wrong way round!!!
+
 for obj in tree.getroot():
     # get the processes
     if (obj.tag=='process'):
+        
+        # need to get the name by matching the participant id above......
+        
+        print('############## NEW PROCESS : {} ###################### '.format(obj.get('name')))
         print (obj.tag)
         for pro in obj:
             dash = '\t --------'+('-'*len(pro.tag))
@@ -32,12 +41,25 @@ for obj in tree.getroot():
             if (pro.tag =='startEvent'):
                 # the id of the outgoing connection / sequenceFlow(i think)
                 print(pro.get('outgoing'))
+                
+            elif (pro.tag =='task'):
+                # the id of the task
+                print('id: {}'.format(pro.get('id')))
+                # the name of the task
+                print('name: {}'.format(pro.get('name')))
+                for flow in pro:
+                    if (flow.tag=='incoming'):
+                        print('{} : {}'.format(flow.tag,flow.text) )
+                    elif (flow.tag=='outgoing'):
+                        print('{} : {}'.format(flow.tag,flow.text) )
+                
             elif (pro.tag =='task'):
                 # the id of the task
                 print(pro.get('id'))
                 # the name of the task
                 print(pro.get('name'))
                 
+        print('############## END PROCESS : {} ###################### \n'.format(obj.get('name')))        
 
 
        
